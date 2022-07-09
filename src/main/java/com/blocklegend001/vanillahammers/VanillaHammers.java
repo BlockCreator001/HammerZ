a0e4b375b088250f35b1b60c7552cadaac2e198a
BlockCreator001 <62543902+BlockCreator001@users.noreply.github.com>
2022-07-09 18:34:38 +0200

    Add files via upload

--- src/main/java/com/blocklegend001/vanillahammers/VanillaHammers.java
+++ src/main/java/com/blocklegend001/vanillahammers/VanillaHammers.java
@@ -32,6 +32,6 @@ public class VanillaHammers
     {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getName());
     }
 }
